db5052fa540b232d9b7edde4d0731a9392a98ab8
Wendel Silva <48558013+wendelsilva@users.noreply.github.com>
2021-12-01 15:50:26 -0300

    wip(challenge-ignite): refactoring components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
 export function Content() {
   // Complete aqui
+  
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
